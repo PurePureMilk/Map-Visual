a0396c829ac17be037091e8f249e5aa2b9bbd6d4
PurePureMilk <gtkpham@gmail.com>
2021-10-03 00:18:11 -0400

    final draft

---
+++ Misc/stringComp.py
@@ -0,0 +1,29 @@
+str1 = "TCCACTGTTTGCGACTAGGT"
+str2 = "TCCACTGTTTACGACTAGGT"
+str3 = "TCCACTGTTCACGACTAGGT"
+str4 = "TCCACTATTTACGATTAGGT"
+str5 = "TCCACTGTTTACGATTAGGT"
+str6 = "TCCATCGTCTGCAATCAGAT"
+
+family = []
+
+'''
+set = {str1, str2, str3, str4, str5, str6};
+array.add(str1)...array.add(str2)
+
+a = 0;
+b = 0; 
+for (a < str.length)
+  for(b < str.length)
+    print(a comp b)
+    print(compareStrand(set[a], set[b]))
+    b++;
+  a++;
+  b = 0;
+
+def compareStrand(dna1, dna2):
+  count = 0
+  for (i = 0; i < str.length; i++)
+    if(dna1[i] == dna2[i])
+      count++;
+'''
